6cac894c83117275296c139b5fb27ee85602f342
Robert Klang <Robert.Klang@RHIMagnesita.com>
2020-12-19 16:34:25 +0100

    Refactor plugins include chain
    Move the selection of which plugins to include
    down to the plugin folder itself.
    
    Add an option to include user local plugins by
    specifying a local_plugins.cmake file, that is
    optionally included

---
+++ plugins/plugin_dev/plugins.cmake
@@ -0,0 +1,27 @@
+#------------------------------------------------------
+#--- When building plugins from LogScrutinizer the
+#--- plugin.cmake is used to define which to include
+#------------------------------------------------------
+
+message("Adding plugins to LogScrutinizer project")
+add_subdirectory(${PLUGIN_PATH}/plugin_example_1)
+add_subdirectory(${PLUGIN_PATH}/plugin_example_2)
+add_subdirectory(${PLUGIN_PATH}/plugin_example_3)
+add_subdirectory(${PLUGIN_PATH}/plugin_example_3_1)
+add_subdirectory(${PLUGIN_PATH}/plugin_example_4)
+add_subdirectory(${PLUGIN_PATH}/plugin_example_5)
+add_subdirectory(${PLUGIN_PATH}/plugin_example_6)
+add_subdirectory(${PLUGIN_PATH}/plugin_tester)
+add_subdirectory(${PLUGIN_PATH}/file_streamer)
+
+if (EXISTS ${PLUGIN_PATH}/local_plugins.cmake)
+    message ("Found ${PLUGIN_PATH}/local_plugins.cmake")
+else()
+    message ("No ${PLUGIN_PATH}/local_plugins.cmake")
+endif()
+
+include (${PLUGIN_PATH}/local_plugins.cmake OPTIONAL)
+
+
+
+
